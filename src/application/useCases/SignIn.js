861c7a76b9759d0083010a8420251f0fda2b09a4
danjul <danielj.quiroga@gmail.com>
2025-05-30 16:36:54 -0400

    Agregando archivos iniciales

---
+++ src/application/useCases/SignIn.js
@@ -0,0 +1,34 @@
+class SignIn {
+  constructor(userRepository, passwordHasher, tokenGenerator) {
+    this.userRepository = userRepository;
+    this.passwordHasher = passwordHasher;
+    this.tokenGenerator = tokenGenerator;
+  }
+ 
+  async execute({ username, password }) {
+    const user = await this.userRepository.findByUsername(username);
+    if (!user) throw new Error('User not found');
+    const isValid = await this.passwordHasher.compare(password, user.password);
+    if (!isValid) throw new Error('Invalid password');
+    const token = this.tokenGenerator.generate({ id: user._id, roles: user.roles });
+    return { user, token };
+  }
+}
+ 
+module.exports = SignIn;
+ 
+const Product = require('../../domain/entities/Product');
+ 
+class CreateProduct {
+  constructor(productRepository) {
+    this.productRepository = productRepository;
+  }
+ 
+  async execute(productData) {
+    const product = new Product(productData);
+    return await this.productRepository.create(product);
+  }
+}
+ 
+module.exports = CreateProduct;
+ 
